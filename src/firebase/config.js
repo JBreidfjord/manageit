0034ca597dc82d8cf53f13007f8d51d1afd3bf99
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-26 15:01:42 -0700

    Setup Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import "firebase/firestore";
+import "firebase/auth";
+
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvoUQgvp46qC7bNS_PlvBmKo89eg_n3SI",
+  authDomain: "project-management-site-f9d8f.firebaseapp.com",
+  projectId: "project-management-site-f9d8f",
+  storageBucket: "project-management-site-f9d8f.appspot.com",
+  messagingSenderId: "863160189566",
+  appId: "1:863160189566:web:1244a2c32ff623f7eb8e2f",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const timestamp = firebase.firestore.Timestamp;
+const auth = firebase.auth();
+
+export { db, auth, timestamp };
