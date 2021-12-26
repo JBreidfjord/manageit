7b439dbc4bf3a3a5383fa34af4416451375d68f7
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-26 15:49:56 -0700

    Create Signup form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
           <NavLink to="/login">Login</NavLink>
         </li>
         <li>
-          <NavLink to="/signup">Signup</NavLink>
+          <NavLink to="/signup">Sign Up</NavLink>
         </li>
         <li>
           <button className="btn">Logout</button>
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,5 +1,46 @@
 import "./Signup.css";
 
+import { useState } from "react";
+
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+
+  return (
+    <form className="auth-form">
+      <h2>Sign Up</h2>
+      <label>
+        <span>Email:</span>
+        <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} required />
+      </label>
+
+      <label>
+        <span>Name:</span>
+        <input
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+          required
+        />
+      </label>
+
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+      </label>
+
+      <label>
+        <span>Avatar:</span>
+        <input type="file" />
+      </label>
+      <button className="btn">Submit</button>
+    </form>
+  );
 }
