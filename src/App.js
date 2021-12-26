07adc3ed261ad80c199886543ac29c50d0e25170
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-26 15:18:24 -0700

    Page templates and Routes setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Create from "./pages/create/Create";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Login from "./pages/login/Login";
+import Project from "./pages/project/Project";
+import Signup from "./pages/signup/Signup";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/projects/:id" element={<Project />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
