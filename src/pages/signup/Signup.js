43dbbdca944f9fc2f6e5ac81b1c476cfd8d986ac
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-26 16:05:06 -0700

    Handle image upload

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -7,9 +7,31 @@ export default function Signup() {
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
   const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+
+    let selected = e.target.files[0];
+
+    if (!selected) {
+      setThumbnailError("Please select a file");
+    } else if (!selected.type.includes("image")) {
+      setThumbnailError("File must be an image");
+    } else if (selected.size > 1000000) {
+      setThumbnailError("File size exceeds 1MB");
+    } else {
+      setThumbnailError(null);
+      setThumbnail(selected);
+    }
+  };
 
   return (
-    <form className="auth-form">
+    <form className="auth-form" onSubmit={handleSubmit}>
       <h2>Sign Up</h2>
       <label>
         <span>Email:</span>
@@ -38,7 +60,8 @@ export default function Signup() {
 
       <label>
         <span>Avatar:</span>
-        <input type="file" />
+        <input type="file" onChange={handleFileChange} required />
+        {thumbnailError && <div className="error">{thumbnailError}</div>}
       </label>
       <button className="btn">Submit</button>
     </form>
