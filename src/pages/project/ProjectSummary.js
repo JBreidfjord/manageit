ae41cbf8d099e7a3e8531d16f82d0476c40a2aa5
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-27 13:02:57 -0700

    Handle deleting projects

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,6 +1,18 @@
 import Avatar from "../../components/Avatar";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useFirestore } from "../../hooks/useFirestore";
+import { useNavigate } from "react-router-dom";
 
 export default function ProjectSummary({ project }) {
+  const { deleteDocument } = useFirestore("projects");
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    deleteDocument(project.id);
+    navigate("/");
+  };
+
   return (
     <div>
       <div className="project-summary">
@@ -16,6 +28,11 @@ export default function ProjectSummary({ project }) {
           ))}
         </div>
       </div>
+      {project.createdBy.id === user.uid && (
+        <button className="btn" onClick={handleClick}>
+          Mark as Complete
+        </button>
+      )}
     </div>
   );
 }
