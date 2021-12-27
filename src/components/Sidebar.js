006ce59a40807623550a1cad9b0f90ccf9d89c28
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-26 18:28:26 -0700

    Avatar component and display user info on Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,20 @@
 import "./Sidebar.css";
 
 import AddIcon from "../assets/add_icon.svg";
+import Avatar from "./Avatar";
 import DashboardIcon from "../assets/dashboard_icon.svg";
 import { NavLink } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Sidebar() {
+  const { user } = useAuthContext();
+
   return (
     <div className="sidebar">
       <div className="sidebar-content">
         <div className="user">
-          {/* {user image and name} */}
-          <p>Welcome $user</p>
+          <Avatar src={user.photoURL} />
+          <p>{user.displayName}</p>
         </div>
         <nav className="links">
           <ul>
